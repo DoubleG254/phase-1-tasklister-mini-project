add472f5e58fee9e35b081ddce56d24faec77ebb
DoubleG254 <glorygweth6@gmail.com>
2023-06-26 22:09:41 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form=document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildingtools(e.target.newtaskdescription.value);
+    form.reset()
+  });
 });
+
+function buildingtools(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", deletetask);
+  btn.textContent = "x";
+  li.textContent = todo
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+function deletetask(e) {
+  e.target.parentNode.remove();
+}
